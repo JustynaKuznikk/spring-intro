c981f61d208dc07ceab00342311f399f5b723c78
unknown <justyna.kuznikk@gmail.com>
2020-10-11 14:24:15 +0200

    HTML

--- projects/adverts/src/main/java/pl/sda/projects/adverts/controller/RegistrationController.java
+++ projects/adverts/src/main/java/pl/sda/projects/adverts/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package pl.sda.projects.adverts.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller @Slf4j
@@ -11,6 +12,10 @@ public class RegistrationController {
 
     @GetMapping
     public String prepareRegistrationPage(){
+        return "";
+    }
+    @PostMapping
+    public String processRegistrationData(){
         return "";
     }
 
