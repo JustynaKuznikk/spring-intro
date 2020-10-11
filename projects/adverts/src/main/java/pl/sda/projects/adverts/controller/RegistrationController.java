19a0f21b7a152a4ddb2437e7c9b643828ca5c6d8
unknown <justyna.kuznikk@gmail.com>
2020-10-11 15:59:38 +0200

    rejestracja użutkownika

--- projects/adverts/src/main/java/pl/sda/projects/adverts/controller/RegistrationController.java
+++ projects/adverts/src/main/java/pl/sda/projects/adverts/controller/RegistrationController.java
@@ -3,6 +3,7 @@ package pl.sda.projects.adverts.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ public class RegistrationController {
 
     private UserRepository userRepository;
 
-    @Autowired
+    @Autowired //userRepository będzie wstrzyknięty przez springa
     public RegistrationController(UserRepository userRepository){
         this.userRepository=userRepository;
     }
@@ -29,7 +30,8 @@ public class RegistrationController {
     public String processRegistrationData(@RequestParam String username,
                                           @RequestParam String password,
                                           @RequestParam String firstName,
-                                          @RequestParam String lastName){
+                                          @RequestParam String lastName,
+                                          ModelMap model){
         User user = User.builder()
                 .username(username)
                 .password(password)
@@ -38,7 +40,10 @@ public class RegistrationController {
         log.debug("User to save: {}",user);
         userRepository.save(user);
         log.info("New user saved: {}", user);
-        return "";
+
+        model.addAttribute("savedUser", user);
+
+        return "registration/registration-summary";
     }
 
 }
